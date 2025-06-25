6862d64446265380399c84f73006bae365c9bfec
ulfatjon <xamrokulovulfatjon2005@gmail.com>
2025-06-25 10:51:52 +0500

    The project is finished.

--- src/pages/Admins/UsersBuild.tsx
+++ src/pages/Admins/UsersBuild.tsx
@@ -156,7 +156,7 @@ const UsersBuild = () => {
         </div>
       )}
 
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto mt-10">
         {build.length === 0 ? (
           <p className="text-center text-gray-500 dark:text-gray-400 py-4">
             O'chirilgan foydalanuvchilar hozircha mavjud emas
